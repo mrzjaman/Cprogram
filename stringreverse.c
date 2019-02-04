eb72d762f887663f526c2cb16bd9cf554eb35ca8
mrzjaman <45986860+mrzjaman@users.noreply.github.com>
2019-02-04 20:02:52 +0530

    Create stringreverse.c

---
+++ stringreverse.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#define MAX 15
+
+int main()
+{
+        char ch[MAX];
+        char temp;
+        int i = 0, j = 0, strlen = 0;
+
+        printf("enter the string you wish to process\n");
+        fgets(ch, MAX, stdin);
+
+        while(ch[i] != '\0')
+        {
+                i++;
+        }
+
+        i = i - 2;
+        strlen = i / 2;
+
+        while(strlen)
+        {
+                temp = ch[j];
+                ch[j] = ch[i];
+                ch[i] = temp;
+                i--;
+                j++;
+                strlen--;
+        }
+
+        printf("Reversed string is %s\n", ch);
+        return 0;
+}
+
